ba5d3b203d5ddba0d58a302752464fdf5ac55d3a
unknown <galaxy.net0710@gmail.com>
2019-09-07 17:02:28 -0500

    Uso de Body y Query

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const router = express.Router()
 
+
 var app = express()
+app.use(bodyParser.json())
 app.use(router)
+
 
 router.get('/message',(req,res)=>{
     res.send('Listas de mensajes')
 })
 
-router.post('/message',(req,res)=>{
-    res.send('Mensaje añadido correctamente')
+router.delete('/message',(req,res)=>{
+    console.log(req.query)
+    console.log(req.body)
+    res.send('Mensaje '+ req.body.text +' añadido correctamente')
 })
 
 // app.use('/',(req,res)=>{
