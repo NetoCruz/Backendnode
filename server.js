6cae1617bda2a4af949a893fc52d1727e8bd39d6
unknown <galaxy.net0710@gmail.com>
2019-09-07 16:28:41 -0500

    Creando un servidor HTTP desde Node

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+var app = express()
+app.use(router)
+
+router.get('/message',(req,res)=>{
+    res.send('Listas de mensajes')
+})
+
+router.post('/message',(req,res)=>{
+    res.send('Mensaje añadido correctamente')
+})
+
+// app.use('/',(req,res)=>{
+//     res.send('Hola')
+// })
+
+app.listen(3000)
+console.log('La app está escuchando en http://localhost:3000')
